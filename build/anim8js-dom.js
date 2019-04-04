9e8e5e6bf806d95d7138c03961f02a7f8226d52d
Philip Diffenderfer <pdiffenderfer@gmail.com>
2019-04-04 10:05:07 -0400

    Fixed UMD

--- build/anim8js-dom.js
+++ build/anim8js-dom.js
@@ -1,11 +1,11 @@
-/* anim8js-dom 1.0.4 - anim8 your HTML elements by Philip Diffenderfer */
+/* anim8js-dom 1.0.6 - anim8 your HTML elements by Philip Diffenderfer */
 // UMD (Universal Module Definition)
 (function (root, factory)
 {
   if (typeof define === 'function' && define.amd) // jshint ignore:line
   {
     // AMD. Register as an anonymous module.
-    define(['anim8'], function(anim8) { // jshint ignore:line
+    define(['anim8js'], function(anim8) { // jshint ignore:line
       return factory(anim8, root);
     });
   }
@@ -14,7 +14,7 @@
     // Node. Does not work with strict CommonJS, but
     // only CommonJS-like environments that support module.exports,
     // like Node.
-    module.exports = factory(require('anim8'), global);  // jshint ignore:line
+    module.exports = factory(require('anim8js'), global);  // jshint ignore:line
   }
   else
   {
--- src/_header.js
+++ src/_header.js
@@ -4,7 +4,7 @@
   if (typeof define === 'function' && define.amd) // jshint ignore:line
   {
     // AMD. Register as an anonymous module.
-    define(['anim8'], function(anim8) { // jshint ignore:line
+    define(['anim8js'], function(anim8) { // jshint ignore:line
       return factory(anim8, root);
     });
   }
@@ -13,7 +13,7 @@
     // Node. Does not work with strict CommonJS, but
     // only CommonJS-like environments that support module.exports,
     // like Node.
-    module.exports = factory(require('anim8'), global);  // jshint ignore:line
+    module.exports = factory(require('anim8js'), global);  // jshint ignore:line
   }
   else
   {
